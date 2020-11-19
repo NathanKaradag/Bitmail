ca88971d232d4d4dbabc50097f64579b0b9f530d
GVisser1 <55433031+GVisser1@users.noreply.github.com>
2020-11-19 16:39:55 +0100

    Added basic UI & functionality to Organisation page

---
+++ Bitmail/Pages/Organisaties.razor.cs
@@ -0,0 +1,84 @@
+﻿using Bitmail.Models;
+using Bitmail.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bitmail.Pages
+{
+    public partial class Organisaties
+    {
+        [Inject]
+        private DatabaseService DatabaseService { get; set; }
+        protected Organisation CurrentOrganisation { get; set; }
+        protected List<Organisation> AllOrganisations { get; set; }
+        protected List<Contact> AllContacts { get; set; }
+        protected bool IsNewOrganisation { get; set; }
+        protected List<int> SelectedContacts { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            AllOrganisations = DatabaseService.DB.Organisations.Include(o=>o.OrganisationContacts).ToList();
+            AllContacts = DatabaseService.DB.Contacts.ToList();
+            CurrentOrganisation = new Organisation();
+        }
+        protected async Task SaveOrganisation()
+        {
+            DatabaseService.DB.Organisations.Add(CurrentOrganisation);
+            
+            List<Contact> realContacts = new List<Contact>();
+            foreach (var selectedContact in SelectedContacts)
+            {
+                Contact existingContact = AllContacts.FirstOrDefault(c => c.Id == selectedContact);
+                realContacts.Add(existingContact);
+            }
+            List<OrganisationContact> res = realContacts.Select(rc => new OrganisationContact() { Organisation = CurrentOrganisation, ContactId=rc.Id, Contact = rc}).ToList();
+            CurrentOrganisation.OrganisationContacts = res;
+            await DatabaseService.DB.SaveChangesAsync();
+            AllOrganisations = DatabaseService.DB.Organisations.ToList();
+            CurrentOrganisation = new Organisation();
+            StateHasChanged();
+        }
+
+        protected void OnOrganisationClicked(Organisation selectedOrganisation)
+        {
+            IsNewOrganisation = false;
+            CurrentOrganisation = selectedOrganisation;
+            SelectedContacts = new List<int>();
+        }
+        protected void OnNewOrganisation()
+        {
+            IsNewOrganisation = true;
+            CurrentOrganisation = new Organisation();
+            SelectedContacts = new List<int>();
+        }
+        protected void OnContactItemSelected(int id)
+        {
+            if(SelectedContacts == null)
+            {
+                SelectedContacts = new List<int>();
+            }
+            if (!SelectedContacts.Contains(id))
+            {
+                SelectedContacts.Add(id);
+            }
+            else
+            {
+                SelectedContacts.Remove(id);
+            }
+            StateHasChanged();
+        }
+        protected void DeleteOrganisation()
+        {
+            DatabaseService.DB.Organisations.Remove(CurrentOrganisation);
+            DatabaseService.DB.SaveChanges();
+            CurrentOrganisation = new Organisation();
+            IsNewOrganisation = true;
+            StateHasChanged();
+            AllOrganisations = DatabaseService.DB.Organisations.Include(o => o.OrganisationContacts).ToList();
+            AllContacts = DatabaseService.DB.Contacts.ToList();
+        }
+    }
+}
