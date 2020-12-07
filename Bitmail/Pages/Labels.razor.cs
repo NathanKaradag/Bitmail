de9039c073de96cb53569ce49f262f8703a59cdd
JeroenUijt <jeroenuijtdewilligen@gmail.com>
2020-12-07 12:42:03 +0100

    Geupdate Tag Zoekfunctie
    Tags zoeken is nu makkelijker
    met hulp van nathan zn code
    thanks he nathan
    10x korter nu ook

---
+++ Bitmail/Pages/Labels.razor.cs
@@ -0,0 +1,25 @@
+﻿using Bitmail.Models;
+using Bitmail.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bitmail.Pages
+{
+    public partial class Labels
+    {
+        [Inject]
+        private DatabaseService DatabaseService { get; set; }
+        
+        protected override void OnInitialized()
+        {
+            alleTags = DatabaseService.DB.Tags.ToList();
+        }
+
+        List<Tag> alleTags = new List<Tag>();
+        string keyword = "";
+        List<Tag> FilteredTags => alleTags.Where(i => (i.Title).ToLower().Contains(keyword.ToLower())).ToList();
+    }
+}
