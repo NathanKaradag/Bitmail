bb14473d9f9b8684e40f183465d89c644ee3c616
nathanXD98 <60744510+nathanXD98@users.noreply.github.com>
2021-01-04 18:43:54 +0100

    contacten in mailchimp, contacten.razor beetje aangepast

--- Bitmail/Pages/Contacten.razor.cs
+++ Bitmail/Pages/Contacten.razor.cs
@@ -6,18 +6,27 @@ using System.Threading.Tasks;
 using Bitmail.Services;
 using Bitmail.Models;
 using Microsoft.EntityFrameworkCore;
+using MailChimpWrapper.Models;
+using Contact = Bitmail.Models.Contact;
+using Tag = Bitmail.Models.Tag;
+using MailChimpWrapper.Models.Exceptions;
+using MailChimpWrapper.Models.Requests;
+using MailChimpWrapper.Models.Responses;
+using Microsoft.Extensions.Configuration;
 
 namespace Bitmail.Pages
 {
     public partial class Contacten
     {
-        [Inject]
-        private DatabaseService DatabaseService { get; set; }
-
+        [Inject] private MailChimpService MailChimpService { get; set; }
+        [Inject] private IConfiguration configuration { get; set; }
+        [Inject] private DatabaseService DatabaseService { get; set; }
         protected Contact CurrentContact { get; set; }
         protected List<Contact> AllContacts { get; set; }
         protected List<Organisation> AllOrganisations { get; set; }
+        protected List<Organisation> Organisations { get; set; }
         protected List<Tag> AllTags { get; set; }
+        protected List<Tag> Tags { get; set; }
         protected bool IsNewContact { get; set; }
         protected List<int> SelectedOrganisations { get; set; }
         protected List<int> SelectedTags { get; set; }
@@ -30,13 +39,16 @@ namespace Bitmail.Pages
             AllContacts = DatabaseService.DB.Contacts.Include(t => t.OrganisationContacts).Include(x => x.ContactTags).ToList();
 
             AllOrganisations = DatabaseService.DB.Organisations.ToList();
+            Organisations = AllOrganisations;
             AllTags = DatabaseService.DB.Tags.ToList();
+            Tags = AllTags;
 
             CurrentContact = new Contact();
         }
 
         protected async Task SaveContact()
         {
+            //await AddContactToMailchimp();
             DatabaseService.DB.Contacts.Add(CurrentContact);
 
             List<Organisation> realOrganisations = new List<Organisation>();
@@ -60,6 +72,7 @@ namespace Bitmail.Pages
             CurrentContact.ContactTags = res2;
             await DatabaseService.DB.SaveChangesAsync();
 
+            await ContactInMailchimp("subscribed");
             AllContacts = DatabaseService.DB.Contacts.ToList();
             CurrentContact = new Contact();
             StateHasChanged();
@@ -117,14 +130,18 @@ namespace Bitmail.Pages
             StateHasChanged();
 
         }
-        protected void RemoveContact()
+        protected async Task RemoveContact()
         {
+            await ContactInMailchimp("unsubscribed");
+
             DatabaseService.DB.Contacts.Remove(CurrentContact);
             DatabaseService.DB.SaveChanges();
+            
             CurrentContact = new Contact();
             IsNewContact = true;
-            StateHasChanged();
 
+            StateHasChanged();
+            
             AllContacts = DatabaseService.DB.Contacts.Include(t => t.OrganisationContacts).Include(x => x.ContactTags).ToList();
             AllOrganisations = DatabaseService.DB.Organisations.ToList();
             AllTags = DatabaseService.DB.Tags.ToList();
@@ -151,12 +168,81 @@ namespace Bitmail.Pages
             CurrentContact.ContactTags = res2;
             await DatabaseService.DB.SaveChangesAsync();
 
+            await ContactInMailchimp("subscribed");
             StateHasChanged();
             EditClicked = false;
         }
         protected void OnEditClicked()
         {
             EditClicked = true;
+        }
+        public async Task ContactInMailchimp(string memberStatus)
+        {
+            try
+            {
+                List<Member> members = new List<Member>();
+                var listId = configuration.GetValue<string>("MailChimpConstants:SubscriberListId");
+
+                Member newMember = new Member();
+                newMember.Status = memberStatus;
+                newMember.MergeFields = new Dictionary<string, object>();
+                newMember.EmailAddress = CurrentContact.Email;
+                newMember.MergeFields = new Dictionary<string, object>(){
+                {"FNAME",CurrentContact.FirstName},
+                {"LNAME",CurrentContact.LastName},
+                };
+                members.Add(newMember);
+
+                var response = await MailChimpService.Client.Request<BatchSubscribeUnsubscribeRequest, BatchSubscribeUnsubscribeResponse>(
+                new BatchSubscribeUnsubscribeRequest(listId)
+                {
+                    Members = members,
+                    UpdateExisting = true,
+                });
+
+                if (CurrentContact.ContactTags != null)
+                {
+                    foreach (var tag in CurrentContact.ContactTags)
+                    {
+                        await MailChimpService.Client.Request(new MemberTagsPostRequest(listId, CurrentContact.Email, new List<MailChimpWrapper.Models.Tag>() { new MailChimpWrapper.Models.Tag() { Name = tag.Tag.Title, Status = "active" } }));
+                    }
+                }
+                
+            }
+            catch (ResponseException responseException)
+            {
+                var message = responseException.ErrorResponse.Detail;
+            }
+            catch (UnknownResponseException unknownException)
+            {
+                var response = unknownException.ResponseMessage;
+            }
+        }
+        protected void OnOrganisationSearch(object value)
+        {
+            if (value != null && !string.IsNullOrEmpty(value.ToString()))
+            {
+                Organisations = AllOrganisations.Where(c =>
+                    (c.Name != null) ? c.Name.ToLower().Contains(value.ToString().ToLower()) : false).ToList();
+            }
+            else
+            {
+                Organisations = AllOrganisations;
+            }
+            StateHasChanged();
+        }
+        protected void OnTagSearch(object value)
+        {
+            if (value != null && !string.IsNullOrEmpty(value.ToString()))
+            {
+                Tags = Tags.Where(c =>
+                    (c.Title != null) ? c.Title.ToLower().Contains(value.ToString().ToLower()) : false).ToList();
+            }
+            else
+            {
+                Tags = AllTags;
+            }
+            StateHasChanged();
         }
     }
 }
